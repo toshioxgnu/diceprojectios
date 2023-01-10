46d2f098ae8b6a6cdd0191393b7e15945cc7a6c0
Jose Gonzalez Riquelme <jgonzalezr5@outlook.es>
2023-01-10 16:40:49 -0300

    Se termina la implementacion del array para la muestra de los dados

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,11 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    let diceImages = [ UIImage(named: "DiceOne"),
+                   UIImage(named: "DiceTwo"),
+                   UIImage(named: "DiceThree"),
+                   UIImage(named: "DiceFour"),
+                   UIImage(named: "DiceFive"),
+                   UIImage(named: "DiceSix"),
+                  ]
+
+    @IBAction func rollButtonPress(_ sender: UIButton) {
+        
+        diceImageView1.image = diceImages[Int.random(in: 0...5)]
+        
+        diceImageView2.image = diceImages[Int.random(in: 0...5)]
+        
     }
-
-
+    
 }
 
